c5fe1848a53ae474092f7d9b981f471349b7624c
ahd0611 <abdulahadkhan282@gmail.com>
2024-08-04 21:55:34 +0530

    first commit

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "ap-south-1"
+}
+variable "key_name" {
+  description = " SSH keys to connect to ec2 instance"
+  default     = "MyKeyPair"     #change key name here
+}
+variable "instance_type" {
+  description = "instance type for ec2"
+  default     = "t2.medium"
+}
+variable "ami_id" {
+  description = "AMI for Ubuntu Ec2 instance"
+  default     = "ami-0ad21ae1d0696ad58"
+}
+variable "bucketname" {
+  description = "The name of the S3 bucket to create"
+  type        = string
+  default     = "abdulahad-khan-cloud-2024"  #change Bucket name also
+}
