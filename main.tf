549f1b88dc3585649f47349d7636d5ab359c16e9
ahd0611 <161612006+ahd0611@users.noreply.github.com>
2024-08-05 01:35:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "Abdulahad" {
   instance_type          = var.instance_type
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
+  subnet_id = var.subnet_id
   user_data              = base64encode(file("website.sh"))
   tags = {
     Name = "Abdulahad-EC2"
